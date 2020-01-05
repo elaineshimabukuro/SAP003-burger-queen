36236afe49841acfcaa4a06c1b46e54dbcf5e4cd
elaineshimabukuro <laineyoshizato@gmail.com>
2020-01-05 10:04:26 -0300

    changed colors layout

--- burger-queen/src/components/header/header.js
+++ burger-queen/src/components/header/header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './style.css';
+import './logo.png';
 
 
 export default function Header (){
     return (
      <header>
-         <h1>Burger <img src="Group 2.png"/>Queen</h1>
+         <h1>Burger <img src="logo.png"/>Queen</h1>
      </header>
     )
   }
