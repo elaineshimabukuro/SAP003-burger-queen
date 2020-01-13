3bbbc48ae2988e7fa7726f06e55c79bb1db62332
elaineshimabukuro <laineyoshizato@gmail.com>
2020-01-13 11:08:08 -0300

    doing extras

--- burger-queen/src/pages/saloon/saloon.js
+++ burger-queen/src/pages/saloon/saloon.js
@@ -5,13 +5,54 @@ import Menu from '../../components/Menu/menu.js'
 import firebase from '../../utils/firebase.js';
 
 
-export default function Saloon(props) {
-    
-    
+export default function Saloon() {
+
     const [itens, setItens]=useState([])
     const [table, setTable]= useState('')
     const [client, setClient]= useState('')
+    const [modal, setModal]= useState({status:false})
+
+
+    const addItens = (newItem) => {
+        const findItem = itens.find(item => item.name === newItem.name)
+        if (findItem) {
+            findItem.qtd++;
+            setItens([...itens])
+            
+        }
+        else {          
+            newItem.qtd = 1;
+            setItens([...itens, newItem])
+        }
+        console.log(itens)
+    }
+
+    const subItem = (product) => {
+        const findItem = itens.find(item => item.name === product.name)
+        if (findItem.qtd > 1) {
+            findItem.qtd--
+            setItens([...itens])
+        }
+    }
+    const deleteItens = (product) => {
+        const index = (itens.indexOf(product))
+        itens.splice(index, 1)
+        setItens([...itens])
+    }
+
+    const total = itens.reduce((acumulador, item) => {
+            return acumulador + (item.price * item.qtd);
+        }, 0)
     
+    const verifyOptions = (menuItem) => {
+        
+        if(menuItem.adicional){
+            setModal({status: true, item: menuItem})
+        } else {
+            addItens(menuItem);
+        }
+    }
+  
     const sendCommand = (e) =>{
         e.preventDefault()
 
@@ -35,11 +76,7 @@ export default function Saloon(props) {
             client,
             table,
             itens: itens.map(function(i) { return { name: i.name, qtd: i.qtd } }),
-            timestamp: new Date().toLocaleString('pt-BR', {
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit'
-              }),
+            timestamp: new Date().toLocaleString('pt-BR'),
             total: total,
             status: 'waiting',
         })
@@ -50,47 +87,30 @@ export default function Saloon(props) {
         )
     }
 
-    const addItens = (newItem) => {
-        const findItem = itens.find(item => item.name === newItem.name)
-        if (findItem) {
-            findItem.qtd++;
-            setItens([...itens])
-        }
-        else {
-            newItem.qtd = 1;
-            setItens([...itens, newItem])
-        }
-    }
-
-    const subItem = (product) => {
-        const findItem = itens.find(item => item.name === product.name)
-        if (findItem.qtd > 1) {
-            findItem.qtd--
-            setItens([...itens])
-        }
-    }
-    const deleteItens = (product) => {
-        const index = (itens.indexOf(product))
-        itens.splice(index, 1)
-        setItens([...itens])
-    }
-
-    const total = itens.reduce((acumulador, item) => {
-            return acumulador + (item.price * item.qtd);
-        }, 0)
-    
-    
-
     return (
         <div class="container">
             <div class="menu">
                 <h1>M e n u</h1>
                     <Menu
-                        handleClick={addItens} />
+                        handleClick={verifyOptions} />
             </div>
+                {modal.status ? (
+                    <div>
+                        <h3>Adicionais</h3>
+                        {modal.item.adicional.map((elem, index) => (
+                            <div key={index}>
+                            <input type='radio' name='adicionais' value={elem.name}/>
+                            <label>{elem.name}</label>
+                            </div>
+                        ))}
+                        <h3>Tipos de Carne</h3>
+                    </div>
+
+                ): false}
+
                 <div class="pedido">
                     <Command
-                        itens={itens}
+                        itens={itens}   
                         setClient={setClient}
                         setTable={setTable}
                         subItem={subItem}
